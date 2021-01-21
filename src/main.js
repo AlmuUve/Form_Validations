a566112372c1cb600dbb49f89005cc36ea70afdc
danseks <danseksfzz@gmail.com>
2021-01-21 16:05:21 +0000

    Fix some issues and add more functions

--- src/main.js
+++ src/main.js
@@ -5,148 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  var luhnChk = (function(arr) {
-    return function(ccNum) {
-      var len = ccNum.length,
-        bit = 1,
-        sum = 0,
-        val;
-      while (len) {
-        val = parseInt(ccNum.charAt(--len), 10);
-        sum += (bit ^= 1) ? arr[val] : val;
-      }
-      return sum && sum % 10 === 0;
-    };
-  })([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]);
-  //"4024007118169068";
-  //CREDIT CARD
-  let creditCard = document.querySelector("#cardNumber");
-  creditCard.addEventListener("focusout", () => {
-    let myCreditCardNum = creditCard.value;
-    let check = false;
-    if (myCreditCardNum.length >= 12 && myCreditCardNum.length <= 19) {
-      check = luhnChk(myCreditCardNum);
-    }
-    if (check == true) {
-      creditCard.classList.remove("border-danger");
-      creditCard.classList.add("border-success");
-    } else {
-      creditCard.classList.remove("border-success");
-      creditCard.classList.add("border-danger");
-    }
-  });
-  //CVC
-  let cvc = document.querySelector("#cvc");
-  cvc.addEventListener("focusout", () => {
-    let myCvcNum = cvc.value;
-    if (parseInt(myCvcNum).toFixed() === myCvcNum) {
-      cvc.classList.remove("border-danger");
-      cvc.classList.add("border-success");
-    } else {
-      cvc.classList.remove("border-success");
-      cvc.classList.add("border-danger");
-    }
-  });
-  //AMOUNT
-  let amount = document.querySelector("#amount");
-  amount.addEventListener("focusout", () => {
-    let myAmountNum = amount.value;
-    console.log(myAmountNum);
-    let convertedAmount = parseFloat(myAmountNum);
-    console.log(convertedAmount);
-    if (
-      convertedAmount > 1000 &&
-      convertedAmount < Number.MAX_SAFE_INTEGER - 1 &&
-      parseFloat(convertedAmount.toFixed(2)) === convertedAmount
-    ) {
-      amount.classList.remove("border-danger");
-      amount.classList.add("border-success");
-    } else {
-      amount.classList.remove("border-success");
-      amount.classList.add("border-danger");
-    }
-  });
-  //FIRST NAME --- PASAR A FUNCION
-  let firstName = document.querySelector("#firstName");
-  firstName.addEventListener("focusout", () => {
-    let myFirstName = firstName.value;
-    let myREGEXonlyletters = /^[a-zA-Z\s]*$/;
-    if (
-      myFirstName.length < 150 &&
-      myFirstName.length > 0 &&
-      myREGEXonlyletters.test(myFirstName)
-    ) {
-      firstName.classList.remove("border-danger");
-      firstName.classList.add("border-success");
-    } else {
-      firstName.classList.remove("border-success");
-      firstName.classList.add("border-danger");
-    }
-  });
-  //LASTNAME // MAKE FUNCTION WITH FIRSTNAME
-  let lastName = document.querySelector("#lastName");
-  let myREGEXonlyletters = /^[a-zA-Z\s]*$/;
-  lastName.addEventListener("focusout", () => {
-    let mylastName = lastName.value;
-    if (
-      mylastName.length < 150 &&
-      mylastName.length > 0 &&
-      myREGEXonlyletters.test(myLastName)
-    ) {
-      lastName.classList.remove("border-danger");
-      lastName.classList.add("border-success");
-    } else {
-      lastName.classList.remove("border-success");
-      lastName.classList.add("border-danger");
-    }
-  });
-  //CITY
-  let city = document.querySelector("#city");
-  let cities = ["madrid", "barcelona"];
-  city.addEventListener("focusout", () => {
-    let myCity = city.value;
-    let validCity = cities.includes(myCity.toLowerCase());
-    if (validCity == true) {
-      city.value = myCity.toLowerCase();
+const creditCard = document.querySelector("#cardNumber");
+const cvc = document.querySelector("#cvc");
 
-      city.value = city.value.charAt(0).toUpperCase() + city.value.slice(1);
-      city.classList.remove("border-danger");
-      city.classList.add("border-success");
-    } else {
-      city.classList.remove("border-success");
-      city.classList.add("border-danger");
-    }
-  });
-  //COMMUNITIES
-  let community = document.querySelector("#community");
-  community.addEventListener("focusout", () => {
-    let myCommunity = community.value;
-    if (myCommunity == 0) {
-      community.classList.remove("border-success");
-      community.classList.add("border-danger");
-    } else {
-      community.classList.remove("border-danger");
-      community.classList.add("border-success");
-    }
-  });
-  //POSTALCODE
-  let postalCode = document.querySelector("#postalCode");
-  let postalCodeList = ["09004", "09003"];
-  postalCode.addEventListener("focusout", () => {
-    let myPostalCode = postalCode.value;
-    console.log(myPostalCode);
-    let validPostalCode = postalCodeList.includes(myPostalCode);
-    if (validPostalCode == true) {
-      postalCode.classList.remove("border-danger");
-      postalCode.classList.add("border-success");
-    } else {
-      postalCode.classList.remove("border-success");
-      postalCode.classList.add("border-danger");
-    }
-  });
+const amount = document.querySelector("#amount");
+
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+
+const city = document.querySelector("#city");
+const community = document.querySelector("#community");
+const postalCode = document.querySelector("#postalCode");
+
+const message = document.querySelector("#message");
+
+const submitCheck = document.querySelector("#checkForm");
+const submitMessage = document.querySelector("#divMessage");
+
+window.onload = function() {
+  checkCreditCard(creditCard);
+  checkCVC(cvc);
+  checkAmount(amount);
+
+  checkFirstAndLastName(firstName);
+  checkFirstAndLastName(lastName);
+
+  checkCity(city);
+  checkCommunity(community);
+  checkPostalCode(postalCode);
+
   //MESSAGE
-  let message = document.querySelector("#message");
   message.addEventListener("focusout", () => {
     let myMessage = message.value;
     let myREGEXmessage = /^[0-9A-Za-zñáéíóúÑÁÉÍÓÚüÜ;\.:'\s\-,!¡¿?)(&%#_çÇ$]/;
@@ -182,3 +70,138 @@ window.onload = function() {
     message.value = myMessage;
   });
 };
+
+console.log(submitCheck);
+
+// SUBMIT
+submitCheck.addEventListener("submit", function(event) {
+  event.preventDefault();
+  let allCheckInputs = document.querySelectorAll(
+    ".form-control, #message, #community"
+  ); ///re-do selector
+  let checkValueInput = false;
+  for (let value of allCheckInputs) {
+    if (value.classList.contains("border-success")) {
+      checkValueInput = true;
+    } else {
+      checkValueInput = false;
+    }
+  }
+  if (checkValueInput == true) {
+    var createP = document.createElement("p");
+    var createMessage = document.createTextNode("Lo conseguiste");
+    createP.appendChild(createMessage); // Tengo que agregar contenido al h1
+    submitMessage.appendChild(createP);
+    submitMessage.classList.remove("d-none");
+    submitMessage.classList.add("alert-success");
+    console.log("holaaaa");
+  } else {
+    var createPerror = document.createElement("p");
+    var createMessageerror = document.createTextNode("No Lo conseguiste");
+    createPerror.appendChild(createMessageerror); // Tengo que agregar contenido al h1
+    submitMessage.appendChild(createPerror);
+    submitMessage.classList.remove("d-none");
+    submitMessage.classList.add("alert-danger");
+    console.log("hola");
+  }
+});
+
+//CREDIT CARD
+function checkCreditCard(input) {
+  input.addEventListener("focusout", () => {
+    let check =
+      luhnChk(input.value) &&
+      input.value.length >= 12 &&
+      input.value.length <= 19;
+    getC(input, check);
+  });
+}
+//CVC
+function checkCVC(input) {
+  input.addEventListener("focusout", () => {
+    let check = parseInt(input.value).toFixed() === input.value;
+    getC(input, check);
+  });
+}
+//AMOUNT
+function checkAmount(input) {
+  input.addEventListener("focusout", () => {
+    let check =
+      parseFloat(input.value) > 1000 &&
+      parseFloat(input.value) < Number.MAX_SAFE_INTEGER - 1 &&
+      parseFloat(parseFloat(input.value).toFixed(2)) ===
+        parseFloat(input.value);
+    getC(input, check);
+  });
+}
+//FIRST AND LAST NAME
+function checkFirstAndLastName(input) {
+  input.addEventListener("focusout", () => {
+    let myREGEXonlyletters = /^[a-zA-Z\s]*$/;
+    let check =
+      input.value.length > 0 &&
+      input.value.length < 150 &&
+      myREGEXonlyletters.test(input.value);
+    getC(input, check);
+  });
+}
+//CITY
+function checkCity(input) {
+  let cities = ["madrid", "barcelona"];
+  input.addEventListener("focusout", () => {
+    let myCity = input.value;
+    let validCity = cities.includes(myCity.toLowerCase());
+    if (validCity == true) {
+      input.value = myCity.toLowerCase();
+
+      input.value = input.value.charAt(0).toUpperCase() + input.value.slice(1);
+      input.classList.remove("border-danger");
+      input.classList.add("border-success");
+    } else {
+      input.classList.remove("border-success");
+      input.classList.add("border-danger");
+    }
+  });
+}
+//COMMUNITY
+function checkCommunity(input) {
+  input.addEventListener("focusout", () => {
+    let check = input.value != 0;
+    getC(input, check);
+  });
+}
+//POSTALCODE
+function checkPostalCode(input) {
+  let postalCodeList = ["09004", "09003"];
+  input.addEventListener("focusout", () => {
+    let check = postalCodeList.includes(input.value);
+    getC(input, check);
+  });
+}
+
+// GET COLORS
+function getC(input, boolean) {
+  if (boolean === true) {
+    input.classList.remove("border-danger");
+    input.classList.add("border-success");
+    return true;
+  } else {
+    input.classList.remove("border-success");
+    input.classList.add("border-danger");
+    return false;
+  }
+}
+
+const luhnChk = (function(arr) {
+  return function(ccNum) {
+    var len = ccNum.length,
+      bit = 1,
+      sum = 0,
+      val;
+    while (len) {
+      val = parseInt(ccNum.charAt(--len), 10);
+      sum += (bit ^= 1) ? arr[val] : val;
+    }
+    return sum && sum % 10 === 0;
+  };
+})([0, 2, 4, 6, 8, 1, 3, 5, 7, 9]);
